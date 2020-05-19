38eeb0f7570e5e9d47bbee81f60fc132dbe63d2f
Suraj0712 <sssapkal@wpi.edu>
2020-05-19 16:45:39 -0400

    upated the script wih launch file

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 xterm  -e  "source /home/sur/home_service_robot/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 
-sleep 5
+sleep 10
 
 xterm  -e  "source /home/sur/home_service_robot/devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
 
-sleep 5
+sleep 10
 
 xterm  -e  "source /home/sur/home_service_robot/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
 
-sleep 5
+sleep 10
 
 xterm  -e  "source /home/sur/home_service_robot/devel/setup.bash; roslaunch pick_objects pick_objects.launch"
